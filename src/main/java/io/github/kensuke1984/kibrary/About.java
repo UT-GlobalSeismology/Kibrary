b58fc12ff50b404d05593c648773bb12886503f1
Kenji Kawai <kenjikawaijp@gmail.com>
2021-08-19 23:11:36 +0900

    Made in where?

--- src/main/java/io/github/kensuke1984/kibrary/About.java
+++ src/main/java/io/github/kensuke1984/kibrary/About.java
@@ -18,6 +18,7 @@ public final class About extends javax.swing.JFrame {
     public static final String CODENAME = "Shiva";
     public static final String VERSION = "0.4.9.31";
     private static final String LINE = "Kibrary " + VERSION + " (" + CODENAME + ")\n" +
+    		"Made in Vana'diel\n" +
             "Copyright \u00a9 2015-2020 Kensuke Konishi and Anselme F.E. Borgeaud.\n\n" +
             "This software is licensed under the GNU General Public License Version 3, 29 June 2007 (https://www.gnu.org/licenses/).\n";
 
