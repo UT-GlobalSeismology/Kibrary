cd1e9915dfe11b40983f7f735fb30e0212aad1c1
otsuruk <otsuru@eps.s.u-tokyo.ac.jp>
2022-05-24 14:35:51 +0900

    some temporary stuff

--- src/main/java/io/github/kensuke1984/kibrary/inv_new/AtaCut_temp.java
+++ src/main/java/io/github/kensuke1984/kibrary/inv_new/AtaCut_temp.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import io.github.kensuke1984.kibrary.inversion.Dvector;
+import io.github.kensuke1984.kibrary.util.MathAid;
 import io.github.kensuke1984.kibrary.visual.MapperShellscript;
 import io.github.kensuke1984.kibrary.voxel.UnknownParameter;
 import io.github.kensuke1984.kibrary.voxel.UnknownParameterFile;
@@ -26,6 +27,59 @@ public class AtaCut_temp {
     private Dvector DVECTOR;
 
     public static void main(String[] args) throws IOException {
+        Path unknownsPath = Paths.get(args[0]);
+        Path ataPath = Paths.get("ata.lst");
+        Path outputPath = Paths.get("sectionAta.lst");
+
+        List<UnknownParameter> parameterList = UnknownParameterFile.read(unknownsPath);
+        List<String> lines = Files.readAllLines(ataPath);
+        double[][] values = new double[lines.size()][lines.size()];
+        for (int i = 0; i < parameterList.size(); i++) {
+            String[] entries = lines.get(i).split("\\s+");
+            for (int j = 0; j < parameterList.size(); j++) {
+                values[i][j] = Double.parseDouble(entries[j]);
+            }
+        }
+
+        for (int i = 0; i < parameterList.size(); i++) {
+            for (int j = 0; j < parameterList.size(); j++) {
+                if (i == j) continue;
+                double coeff = values[i][j] * values[i][j] / values[i][i] / values[j][j];
+                if (values[i][i] > Double.parseDouble(args[1]) && coeff > Double.parseDouble(args[2])) {
+                    System.out.println(i + " " + j + " " + values[i][i] + " " + values[i][j] + " " + coeff);
+                    System.out.println("  " + parameterList.get(i).getPosition() + " " + parameterList.get(j).getPosition());
+                }
+            }
+        }
+        try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(outputPath))) {
+
+            int iAccu = 0;
+            for (int i = 0; i < parameterList.size(); i++) {
+                UnknownParameter unknown = parameterList.get(i);
+
+                if (MathAid.equalWithinEpsilon(unknown.getPosition().getR(), 3505, 1)) {
+                    iAccu++;
+                    String[] entries = lines.get(i).split("\\s+");
+
+                    int jAccu = 0;
+                    for (int j = 0; j < parameterList.size(); j++) {
+                        UnknownParameter unknownJ = parameterList.get(j);
+
+                        if (MathAid.equalWithinEpsilon(unknownJ.getPosition().getR(), 3505, 1)) {
+                            jAccu++;
+                            int base = 5;
+                            if (iAccu > base && jAccu > base) {
+                                pw.println((iAccu - base) + " " + (jAccu - base) + " " + entries[j]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static void extractColumn(String[] args) throws IOException {
         Path unknownsPath = Paths.get(args[0]);
         String nameRoot = args[1];
         Path valuesPath = Paths.get(nameRoot + ".lst");
--- src/main/java/io/github/kensuke1984/kibrary/inv_new/Multigrid_temp.java
+++ src/main/java/io/github/kensuke1984/kibrary/inv_new/Multigrid_temp.java
@@ -1,10 +1,13 @@
 package io.github.kensuke1984.kibrary.inv_new;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import io.github.kensuke1984.kibrary.util.MathAid;
 import io.github.kensuke1984.kibrary.util.earth.FullPosition;
@@ -48,7 +51,26 @@ public class Multigrid_temp {
             UnknownParameterFile.write(newList, Paths.get("unknowns_multi.lst"));
         }
 
+        if (mode.equals("-m")) {
+            for (int i = 1; i <= 20; i++) {
+                Path vsPath = Paths.get("CG" + i).resolve("vsPercent.lst");
+                List<String> lines = Files.readAllLines(vsPath);
+                List<String> newLines = new ArrayList<>();
 
+                for (String line : lines) {
+                    double[] values = Stream.of(line.trim().split("\\s+")).mapToDouble(Double::parseDouble).toArray();
+                    newLines.add(line);
+
+                    if (values[2] < 3610 && values[0] < -34 && MathAid.equalWithinEpsilon(values[1], -25, 0.1)) {
+                        newLines.add(values[0] + " -20.0000 " + values[2] + " " + values[3]);
+                    }
+                }
+
+                try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(vsPath))) {
+                    newLines.stream().sorted().forEach(pw::println);
+                }
+            }
+        }
 
     }
 }
