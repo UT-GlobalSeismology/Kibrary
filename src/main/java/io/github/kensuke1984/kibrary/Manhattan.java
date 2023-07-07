fbbcdc9f2e40f7a94a524a5533da0110c62518e3
Rei310 <rei_sato@eps.s.u-tokyo.ac.jp>
2023-07-07 17:07:32 +0900

    Implement SyntheticWaveformPlotter class

--- src/main/java/io/github/kensuke1984/kibrary/Manhattan.java
+++ src/main/java/io/github/kensuke1984/kibrary/Manhattan.java
@@ -41,6 +41,7 @@ import io.github.kensuke1984.kibrary.visual.plot.DataFeatureHistogram;
 import io.github.kensuke1984.kibrary.visual.plot.PartialWaveformPlotter;
 import io.github.kensuke1984.kibrary.visual.plot.PolynomialStructurePlotter;
 import io.github.kensuke1984.kibrary.visual.plot.SyntheticRecordSectionCreator;
+import io.github.kensuke1984.kibrary.visual.plot.SyntheticWaveformPlotter;
 import io.github.kensuke1984.kibrary.voxel.VoxelLayoutDesigner;
 import io.github.kensuke1984.kibrary.waveform.ActualWaveformCompiler;
 import io.github.kensuke1984.kibrary.waveform.BasicIDMerge;
@@ -74,6 +75,7 @@ enum Manhattan {
     SyntheticDSMSetup(20, SyntheticDSMSetup.class),
     SPC_SAC(21, SPC_SAC.class),
     SyntheticRecordSectionCreator(22, SyntheticRecordSectionCreator.class),
+    SyntheticWaveformPlotter(23, SyntheticWaveformPlotter.class),
     // Filtered 30
     FilterDivider(30, FilterDivider.class),
     TimewindowMaker(31, TimewindowMaker.class),
