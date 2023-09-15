7056c6c424e7c8ef716e4ff0a767f353a5359e97
Rei310 <rei_sato@eps.s.u-tokyo.ac.jp>
2023-09-15 12:23:43 +0900

    Revise SingleRayCorrection

--- src/main/java/io/github/afeborgeaud/tomotool/raytheory/SingleRayCorrection.java
+++ src/main/java/io/github/afeborgeaud/tomotool/raytheory/SingleRayCorrection.java
@@ -22,12 +22,12 @@ import io.github.kensuke1984.kibrary.util.GadgetAid;
 
 /**
  * Compute mantle correction value at each raypath.
- * Different from MantleCorrection, this class uses single phase to compute correction value. 
+ * Different from MantleCorrection, this class uses single phase to compute correction value.
  * Raypath informations are referred from {@link TimewindowData}.
- * 
+ *
  * The format of output file is same as {@link StaticCorrectionDataFile}.
  * bouncepoint.lst file includes following: obsever, event, bounce point position, and mantle correction value.
- * 
+ *
  * @author ryoichi
  */
 
@@ -81,9 +81,9 @@ public class SingleRayCorrection{
      * Information of bounce points
      */
     private Set<String> bounces = new HashSet<>();
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         if(args.length != 3) {
             System.err.println("USAGE:");
             System.err.println("timewindow file path, 3Dmodel, phase");
@@ -92,26 +92,26 @@ public class SingleRayCorrection{
         } else {
             long startTime = System.nanoTime();
             System.err.println(SingleRayCorrection.class.getName() + " is going...");
-            
+
             SingleRayCorrection src = new SingleRayCorrection(Paths.get(args[0]), args[1].trim(), args[2].trim());
-            
+
             src.run();
-            
+
             System.err.println(SingleRayCorrection.class.getName() + " finished in " + GadgetAid.toTimeString(System.nanoTime() - startTime));
         }
     }
-    
+
     public SingleRayCorrection(Path inputPath, String threedModel, String phase) throws IOException {
         this.inputPath = inputPath;
         this.threeDmodel = threedModel;
         this.phaseName = phase;
-        
+
         set();
     }
-    
+
     private void set() throws IOException {
         timewindows = TimewindowDataFile.read(inputPath);
-        
+
         // set 3D seismic model
         switch (threeDmodel) {
         case "semucb" :
@@ -124,7 +124,7 @@ public class SingleRayCorrection{
             throw new RuntimeException("Error: 3D model " + threeDmodel + " not implemented yet");
         }
         seismic3Dmodel.setTruncationRange(6011., 6371.); // TODO
-        
+
         switch (phaseName) {
         case "S" :
             phaseNames = "S";
@@ -135,15 +135,15 @@ public class SingleRayCorrection{
             phase = new Phase[] {Phase.P};
             break;
         default :
-            throw new RuntimeException("Error; phase " + phaseName + " not implemented yet");
+            throw new RuntimeException("Error: phase " + phaseName + " not implemented yet");
         }
-        
+
         modelName = "prem";
     }
-    
+
     private void run() throws IOException {
         setOutput();
-        
+
         if(timewindows == null || timewindows.isEmpty()) {
             throw new RuntimeException("Error: timewindow file is null or empty");
         } else {
@@ -151,10 +151,10 @@ public class SingleRayCorrection{
                 Compute(window);
             }
         }
-        
+
         writeResult();
     }
-    
+
     /**
      * Set path to output file and bounce point file
      */
@@ -162,17 +162,17 @@ public class SingleRayCorrection{
         switch (phaseName) {
         case "S" :
             bouncepointPath = Paths.get(DatasetAid.generateOutputFileName("bouncepoint_S", threeDmodel, GadgetAid.getTemporaryString(), ".lst"));
-            outPath = Paths.get(DatasetAid.generateOutputFileName("singleRayCorrection_S", threeDmodel, GadgetAid.getTemporaryString(), ".dat"));        
+            outPath = Paths.get(DatasetAid.generateOutputFileName("singleRayCorrection_S", threeDmodel, GadgetAid.getTemporaryString(), ".dat"));
             break;
         case "P" :
             bouncepointPath = Paths.get(DatasetAid.generateOutputFileName("bouncepoint_P", threeDmodel, GadgetAid.getTemporaryString(), ".lst"));
-            outPath = Paths.get(DatasetAid.generateOutputFileName("singleRayCorrection_P", threeDmodel, GadgetAid.getTemporaryString(), ".dat"));        
+            outPath = Paths.get(DatasetAid.generateOutputFileName("singleRayCorrection_P", threeDmodel, GadgetAid.getTemporaryString(), ".dat"));
             break;
         default :
             throw new RuntimeException("Error: phase " + phaseName + " not implemented yet");
         }
     }
-    
+
     /**
      * Compute mantle correction value. Computation is excuted by using {@link Traveltime}.
      * @param window
@@ -181,37 +181,34 @@ public class SingleRayCorrection{
     public void Compute(TimewindowData window) throws IOException {
         List<RaypathInformation> raypathInformations = new ArrayList<>();
         raypathInformations.add(new RaypathInformation(window.getObserver(), window.getGlobalCMTID()));
-        
+
         Traveltime traveltimetool = new Traveltime(raypathInformations, modelName, seismic3Dmodel, phaseNames);
-        
+
         traveltimetool.setIgnoreMantle(false);
         traveltimetool.setIgnoreCMBElevation(true);
         traveltimetool.run();
-        
+
         List<List<TraveltimeData>> ttData = traveltimetool.getMeasurements();
-        
-        List<TraveltimeData> ttData_direct = new ArrayList<>();
-        
+
+//        List<TraveltimeData> ttData_direct = new ArrayList<>();
+
         for(List<TraveltimeData> record : ttData) {
             Set<String> phases = record.stream().map(p -> p.getPhaseName()).collect(Collectors.toSet());
             if(phaseName.equals("P") && !(phases.contains("P"))) continue;
             if(phaseName.equals("S") && !(phases.contains("S"))) continue;
+//            TraveltimeData ttdDir = null;
             TraveltimeData ttdDir = null;
-            for (TraveltimeData ttd : record) {
-                if(phaseName.equals("P")) {
-                    if(ttd.getPhaseName().equals("P")) {
-                        ttdDir = ttd;
-                        ttData_direct.add(ttd);
-                    }
-                }
-                if(phaseName.equals("S")) {
-                    if(ttd.getPhaseName().equals("S")) {
-                        ttdDir = ttd;
-                        ttData_direct.add(ttd);                    }
+            TraveltimeData ttd = null;
+            for (int i = 0; i < record.size(); i++) {
+                if(ttd.getPhaseName().equals(phaseName)) {
+                    ttd = record.get(i);
+                    if (i == 0) ttdDir = ttd;
+                    else if(ttd.getAbsoluteTraveltimePREM() < ttdDir.getAbsoluteTraveltimePREM()) ttdDir = ttd;
+//                      ttData_direct.add(ttd);
                 }
             }
             double shift = ttdDir.getTraveltimePerturbationToPREM();
-            
+
             StaticCorrectionData correction = new StaticCorrectionData(ttdDir.getObserver(), ttdDir.getGlobalCMTID()
                     , window.getComponent(), 0., shift, 1., phase);
             corrections.add(correction);
