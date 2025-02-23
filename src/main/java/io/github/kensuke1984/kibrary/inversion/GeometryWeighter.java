50519fc50c21bdc1343b7574f98f4b2dabb79fd7
otsuruk <otsuru@eps.s.u-tokyo.ac.jp>
2025-02-23 21:18:43 +0900

    GeometryWeighter: also weight by turning point position

--- src/main/java/io/github/kensuke1984/kibrary/inversion/GeometryWeighter.java
+++ src/main/java/io/github/kensuke1984/kibrary/inversion/GeometryWeighter.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,6 +28,7 @@ import io.github.kensuke1984.kibrary.util.data.DataEntryListFile;
 import io.github.kensuke1984.kibrary.util.earth.FullPosition;
 import io.github.kensuke1984.kibrary.util.earth.HorizontalPosition;
 import io.github.kensuke1984.kibrary.util.sac.SACComponent;
+import io.github.kensuke1984.kibrary.voxel.VoxelInformationFile;
 
 /**
  * Weight data based on (bounce point, distance, azimuth)-pair bins.
@@ -58,6 +60,10 @@ public class GeometryWeighter extends Operation {
      * Path of the input data entry list file.
      */
     private Path dataEntryPath;
+    /**
+     * Path of a voxel information file.
+     */
+    private Path voxelPath;
 
     private double distanceInterval;
     private double azimuthInterval;
@@ -75,6 +81,11 @@ public class GeometryWeighter extends Operation {
     private LinearRange distanceRange;
     private CircularRange azimuthRange;
 
+    /**
+     * Horizontal positions of center points of voxels.
+     */
+    private List<HorizontalPosition> voxelPositions;
+    private double[] latitudes;
 
     /**
      * @param args (String[]) Arguments: none to create a property file, path of property file to run it.
@@ -100,14 +111,14 @@ public class GeometryWeighter extends Operation {
             pw.println("#components ");
             pw.println("##Path of a data entry list file, must be set.");
             pw.println("#dataEntryPath dataEntry.lst");
-
+            pw.println("##Path of a voxel information file, when weighting by turning point position.");
+            pw.println("#voxelPath voxel.inf");
             pw.println("##(double) The width of each epicentral distance bin [deg]. (5)");
             pw.println("#distanceInterval ");
             pw.println("##(double) The width of each azimuth bin [deg]. (30)");
             pw.println("#azimuthInterval ");
             pw.println("##(boolean) Whether to expand azimuth range to [0:360), not overlapping onto [0:180) range. (false)");
             pw.println("#expandAzimuth ");
-
             pw.println("##########Parameters for turning point computation##########");
             pw.println("##(String) Name of structure to use for calculating turning point. (prem)");
             pw.println("#structureName ");
@@ -139,6 +150,9 @@ public class GeometryWeighter extends Operation {
                 .map(SACComponent::valueOf).collect(Collectors.toSet());
 
         dataEntryPath = property.parsePath("dataEntryPath", null, true, workPath);
+        if (property.containsKey("voxelPath")) {
+            voxelPath = property.parsePath("voxelPath", null, true, workPath);
+        }
 
         distanceInterval = property.parseDouble("distanceInterval", "5");
         azimuthInterval = property.parseDouble("azimuthInterval", "30");
@@ -160,6 +174,12 @@ public class GeometryWeighter extends Operation {
         Set<DataEntry> entrySet = DataEntryListFile.readAsSet(dataEntryPath).stream()
                 .filter(entry -> components.contains(entry.getComponent())).collect(Collectors.toSet());
 
+        // read voxel information
+        if (voxelPath != null) {
+            VoxelInformationFile vif = new VoxelInformationFile(voxelPath);
+            voxelPositions = vif.getHorizontalPositions();
+            latitudes = voxelPositions.stream().mapToDouble(HorizontalPosition::getLatitude).distinct().sorted().toArray();
+        }
 
         // compute turning point azimuth using TauPPierce
         TauPPierceWrapper pierceTool = null;
@@ -174,9 +194,11 @@ public class GeometryWeighter extends Operation {
         int nDistanceBin = (int) MathAid.ceil(360 / distanceInterval);
         int azimuthDomainWidth = expandAzimuth ? 360 : 180;
         int nAzimuthBin = (int) MathAid.ceil(azimuthDomainWidth / azimuthInterval);
-        int[][] numberOfRecords = new int[nDistanceBin][nAzimuthBin];
+        int nVoxelBin = (voxelPositions != null ? voxelPositions.size() : 1);
+        int[][][] numberOfRecords = new int[nDistanceBin][nAzimuthBin][nVoxelBin];
         Map<DataEntry, Integer> iDistanceMap = new HashMap<>();
         Map<DataEntry, Integer> iAzimuthMap = new HashMap<>();
+        Map<DataEntry, Integer> iVoxelMap = new HashMap<>();
 
         // count number of records in each bin
         for (DataEntry entry : entrySet) {
@@ -185,11 +207,13 @@ public class GeometryWeighter extends Operation {
             double epicentralDistance = eventPosition.computeEpicentralDistanceDeg(observerPosition);
 
             double azimuth;
+            HorizontalPosition turnPosition;
             if (pierceTool.hasRaypaths(entry)) {
                 // When there are several raypaths for a given phase name, the first arrival is chosen.
                 // When there are multiple bottoming points for a raypath, the first one is used.
                 // Any phase (except for "p" or "s") should have a bottoming point, so a non-existence is not considered.
                 azimuth = pierceTool.get(entry, 0).computeTurningAzimuthDeg(0);
+                turnPosition = pierceTool.get(entry, 0).findTurningPoint(0);
             } else {
                 System.err.println("Cannot compute turning point for " + entry + ", skipping.");
                 continue;
@@ -198,21 +222,23 @@ public class GeometryWeighter extends Operation {
 
             int iDistance = (int) (epicentralDistance / distanceInterval);
             int iAzimuth = (int) (azimuth / azimuthInterval);
-            numberOfRecords[iDistance][iAzimuth]++;
+            int iVoxel = (voxelPositions != null ? findIVoxel(turnPosition) : 0);
+            numberOfRecords[iDistance][iAzimuth][iVoxel]++;
 
             iDistanceMap.put(entry, iDistance);
             iAzimuthMap.put(entry, iAzimuth);
+            iVoxelMap.put(entry, iVoxel);
         }
 
         // decide weights
-        double[][] weights = decideWeights(numberOfRecords);
+        double[][][] weights = decideWeights(numberOfRecords);
         Map<DataEntry, Double> weightMap = new HashMap<>();
         for (DataEntry entry : entrySet) {
-            if (!iAzimuthMap.containsKey(entry)) {
-                // If raypath with no azimuth information exists, weights cannot be computed.
+            if (!iDistanceMap.containsKey(entry) || !iAzimuthMap.containsKey(entry) || !iVoxelMap.containsKey(entry)) {
+                // If raypath with no bin information exists, weights cannot be computed.
                 throw new IllegalStateException("No information for " + entry + ".");
             }
-            double weight = weights[iDistanceMap.get(entry)][iAzimuthMap.get(entry)];
+            double weight = weights[iDistanceMap.get(entry)][iAzimuthMap.get(entry)][iVoxelMap.get(entry)];
             weightMap.put(entry, weight);
         }
 
@@ -224,43 +250,82 @@ public class GeometryWeighter extends Operation {
         EntryWeightListFile.write(weightMap, weightPath);
     }
 
-    private static void writeHistogramData(Path txtPath, double distanceInterval, double azimuthInterval,
-            int[][] numberOfRecords, double[][] weights) throws IOException {
+    private int findIVoxel(HorizontalPosition turnPosition) {
+        // decide latitude
+        double latitude = findClosest(turnPosition.getLatitude(), latitudes);
+
+        // extract longitudes on that latitude
+        double[] longitudes = voxelPositions.stream().filter(pos -> pos.getLatitude() == latitude)
+                .mapToDouble(HorizontalPosition::getLongitude).distinct().sorted().toArray();
+        // decide longitude
+        double longitude = findClosest(turnPosition.getLongitude(), longitudes);
+
+        // find index of voxel position
+        HorizontalPosition position = new HorizontalPosition(latitude, longitude);
+        return voxelPositions.indexOf(position);
+    }
+
+    private static double findClosest(double x, double[] values) {
+        double tmpValue = values[0];
+        for (double value : values) {
+            if (Math.abs(value - x) < Math.abs(tmpValue - x)) tmpValue = value;
+        }
+        return tmpValue;
+    }
+
+    private void writeHistogramData(Path txtPath, double distanceInterval, double azimuthInterval,
+            int[][][] numberOfRecords, double[][][] weights) throws IOException {
         try (PrintWriter pw = new PrintWriter(Files.newBufferedWriter(txtPath))) {
             for (int i = 0; i < numberOfRecords.length; i++) {
                 for (int j = 0; j < numberOfRecords[i].length; j++) {
-                    pw.println(String.format("%.2f %.2f %d %.1f", i * distanceInterval, j * azimuthInterval,
-                            numberOfRecords[i][j], numberOfRecords[i][j] * weights[i][j]));
+                    for (int k = 0; k < numberOfRecords[i][j].length; k++) {
+
+                        double distance = i * distanceInterval;
+                        double azimuth = j * azimuthInterval;
+                        if (distanceRange.check(distance) && azimuthRange.check(azimuth)) {
+                            pw.println(String.format("%.2f %.2f %d %d %.1f", distance, azimuth, k,
+                                    numberOfRecords[i][j][k], numberOfRecords[i][j][k] * weights[i][j][k]));
+                        }
+
+                    }
                 }
             }
         }
     }
 
-    private static double[][] decideWeights(int[][] numberOfRecords) {
-        double[][] weights = new double[numberOfRecords.length][numberOfRecords[0].length];
+    private static double[][][] decideWeights(int[][][] numberOfRecords) {
+        double[][][] weights = new double[numberOfRecords.length][numberOfRecords[0].length][numberOfRecords[0][0].length];
 
         // average number of records in each bin (only bins with at least 1 record are considered)
         int sum = 0;
         int nBin = 0;
         for (int i = 0; i < numberOfRecords.length; i++) {
             for (int j = 0; j < numberOfRecords[i].length; j++) {
-                if (numberOfRecords[i][j] > 0) {
-                    sum += numberOfRecords[i][j];
-                    nBin++;
+                for (int k = 0; k < numberOfRecords[i][j].length; k++) {
+
+                    if (numberOfRecords[i][j][k] > 0) {
+                        sum += numberOfRecords[i][j][k];
+                        nBin++;
+                    }
+
                 }
             }
         }
         double average = ((double) sum) / nBin;
 
         // weight for each bin
-        for (int i = 0; i < weights.length; i++) {
-            for (int j = 0; j < weights[i].length; j++) {
-                if (numberOfRecords[i][j] > 0) {
-                    double x = numberOfRecords[i][j] / average;
-                    double weight = (1.0 - Math.exp(-3.0 * x)) / (1.0 - Math.exp(-3.0)) / x;
-                    weights[i][j] = Precision.round(weight, 3);
-                } else {
-                    weights[i][j] = 0.0;
+        for (int i = 0; i < numberOfRecords.length; i++) {
+            for (int j = 0; j < numberOfRecords[i].length; j++) {
+                for (int k = 0; k < numberOfRecords[i][j].length; k++) {
+
+                    if (numberOfRecords[i][j][k] > 0) {
+                        double x = numberOfRecords[i][j][k] / average;
+                        double weight = (1.0 - Math.exp(-3.0 * x)) / (1.0 - Math.exp(-3.0)) / x;
+                        weights[i][j][k] = Precision.round(weight, 3);
+                    } else {
+                        weights[i][j][k] = 0.0;
+                    }
+
                 }
             }
         }
